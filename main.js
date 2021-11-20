8650a885b123126fe2679e127c5c448b2ccdbe72
masta <mastanilewicz@gmail.com>
2021-11-20 23:33:26 +0000

    added enemies with func

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let score = 0;
 let gameFrame = 0;
 ctx.font = '50px Georgia';
 let gameSpeed = 1;
+let gameOver = false;
 
 // Mouse Interactivity
 
@@ -55,6 +56,20 @@ class Player {
         if (mouse.y != this.y) {
             this.y -= dy / 20;
         }
+        if (gameFrame % 5 == 0) {
+            this.frame++;
+            if (this.frame >= 12) this.frame = 0; //cycles though sprite imgs cut offs
+            if (this.frame == 3 || this.frame == 7 || this.frame == 11) {    // @ | @ | @ | @ (3)
+                this.frameX = 0;                                             // @ | @ | @ | @ (7)
+            } else {                                                         // @ | @ | @ | @ (11)
+                this.frameX++;
+            }
+            if (this.frame < 3) this.frameY = 0;
+            else if (this.frame < 7) this.frameY = 1;
+            else if (this.frame < 11) this.frameY = 2;
+            else this.frameY = 0;
+        }
+
     }
     draw() {
         if (mouse.click) {   /* rysuje linie od pozycji playera do myszki podczas mouse.click = True */
@@ -147,8 +162,74 @@ function handleBubbles() {
         }
     }
 }
-for (let i = 0; i < bubbleArray.length; i++) {
 
+// Enemies
+const enemyImage = new Image();
+enemyImage.src = "assets/Fishgameasset/spritesheets/__green_cartoon_fish_01_swim.png"
+
+class Enemy {
+    constructor() {
+        this.x = canvas.width + 200;
+        this.y = Math.random() * (canvas.height - 150) + 90
+        this.radius = 60;
+        this.speed = Math.random() * 2 + 2;
+        this.frame = 0;
+        this.frameX = 0; // will cycle between 0 1 2 3 as there are 4 img in col
+        this.frameY = 0; // will cycle between 0 1 2 as there are 3 img in row on sprite sheet
+        this.spriteWidth = 418; //devides sprite to get single img
+        this.spriteHeight = 397;
+    }
+    //draw circle to use in collisions
+    draw() {
+        // ctx.fillStyle = 'red';
+        // ctx.beginPath();
+        // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        // ctx.fill(); // what↓↓        cut from sprite on x ↓↓    |     cut from sprite on y ↓↓  |    size of cut x ↓↓ | size of cut y ↓↓ |    pos of img ↓↓      |    width and height of img ↓↓
+        ctx.drawImage(enemyImage, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x - 60, this.y - 70, this.spriteWidth / 3, this.spriteHeight / 3)
+    }
+    update() {
+        this.x -= this.speed; // moves enemy to left on each frame based on this.speed
+        if (this.x < 0 - this.radius * 2) { // if enemy is out of window:
+            this.x = canvas.width + 200;    //reset his pos with new random this.y and this.speed
+            this.y = Math.random() * (canvas.height - 150) + 90;
+            this.speed = Math.random() * 2 + 2;
+        }
+
+
+        // enemy img animation on every 5th frame
+        if (gameFrame % 5 == 0) {
+            this.frame++;
+            if (this.frame >= 12) this.frame = 0; //cycles though sprite imgs cut offs
+            if (this.frame == 3 || this.frame == 7 || this.frame == 11) {    // @ | @ | @ | @ (3)
+                this.frameX = 0;                                             // @ | @ | @ | @ (7)
+            } else {                                                         // @ | @ | @ | @ (11)
+                this.frameX++;
+            }
+            if (this.frame < 3) this.frameY = 0;
+            else if (this.frame < 7) this.frameY = 1;
+            else if (this.frame < 11) this.frameY = 2;
+            else this.frameY = 0;
+        }
+
+        // enemy collision
+        const dx = this.x - player.x; // distance on x axis from enemy to player
+        const dy = this.y - player.y;
+        const distance = Math.sqrt(dx * dx + dy * dy) // pitagoras
+        if (distance < this.radius + player.radius) {
+            handleGameOver();
+        }
+    }
+}
+const enemy1 = new Enemy(); // creates actual enemy based on Enemy class
+function handleEnemies() {
+    enemy1.draw();
+    enemy1.update();
+}
+
+function handleGameOver() {
+    ctx.fillStyle = 'white';
+    ctx.fillText('Game Over. score: ' + score, 130, 250);  // (text + pos of text)
+    gameOver = true;
 }
 
 // Repeating backgrounds 
@@ -183,10 +264,11 @@ function animate() {
     handleBubbles();
     player.update();
     player.draw();
+    handleEnemies()
     ctx.fillStyle = 'black';
     ctx.fillText('score: ' + score, 10, 50)
     gameFrame++;
-    requestAnimationFrame(animate);
+    if (!gameOver) requestAnimationFrame(animate); // if not game over keep animate
 
 }
 animate();
